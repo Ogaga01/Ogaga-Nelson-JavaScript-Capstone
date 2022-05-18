99613b14b27b4ec06cfeb96dc4fd8b8626d2b1eb
mandelatuks <manderat206@gmail.com>
2022-05-18 11:02:57 +0200

    Add base url for API we are working with

--- src/index.js
+++ src/index.js
@@ -8,3 +8,18 @@ document.querySelector('#logo_img').setAttribute('src', logo);
 
 const artContainer = document.getElementById('art-container');
 const artContent = document.createElement('div');
+
+let images = [];
+
+const baseUrl = 'https://api.artic.edu/api/v1/artworks?limit=15';
+const getImages = async () => {
+  const response = await fetch(baseUrl);
+  const data = await response.json();
+  images = data.data;
+  images = images.map((image) => ({
+    id: image.id,
+    image_id: image.image_id,
+    title: image.title,
+    date: image.date_start,
+    artist: image.artist_title,
+  })).filter((image) => image.image_id !== null && image.artist !== null);
