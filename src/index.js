c09373ac1b738d8aeb701bcb6513c425855d39a8
Ogaga <iyaraogaga69@gmail.com>
2022-05-18 09:32:33 +0100

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './styles.css';
 import img from './assets/OD-image.png';
 
-
-const logo = document.getElementById('logo1'); 
+const logo = document.getElementById('logo1');
 logo.src = img;
