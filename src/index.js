76110ad15a76e67c9256b8ea49c1aa5e527ad73c
Ogaga <iyaraogaga69@gmail.com>
2022-05-18 10:12:31 +0100

    Add art from Api dynamically to the homepage

--- src/index.js
+++ src/index.js
@@ -23,3 +23,17 @@ const getImages = async () => {
     date: image.date_start,
     artist: image.artist_title,
   })).filter((image) => image.image_id !== null && image.artist !== null);
+
+  const imageString = images.map((img, index) => `
+  <article class="article-style">
+     <h2 class="title">${img.title},
+       ${img.date}</h2>
+     <h3 class="artist">${img.artist}</h3>
+     <img class="image-style" src="https://www.artic.edu/iiif/2/${img.image_id}/full/843,/0/default.jpg"
+       alt="image of artwork">
+     <figure class="caption-container">
+       <figcaption class="caption-content">
+         <img class="like" id="${img.id}" src="/src/heart-empty.png" alt="like icon">&nbsp;
+           <span class="like-count">
+          
+           </span>
