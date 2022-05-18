acc3466f20a3d12130684d25c53b02a4c6c9279b
mandelatuks <manderat206@gmail.com>
2022-05-18 11:01:03 +0200

    Edit  to the html and add main section for art

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import img from './assets/OD-image.png';
 
 const logo = document.getElementById('logo1');
 logo.src = img;
+
+// document.querySelector('#logo_img').setAttribute('src', logo);
+
+// const artContainer = document.getElementById('art-container');
+// const artContent = document.createElement('div');
