11e98c51193b64b061ea365815383310b83c56c3
Ogaga <iyaraogaga69@gmail.com>
2022-05-19 16:26:41 +0100

    Add like count test

---
+++ src/__mocks__/fetchAPI.js
@@ -0,0 +1,99 @@
+const likesList = () => {
+    const likes = [
+        {
+        "likes": 1,
+        "item_id": "item1"
+        },
+        {
+        "item_id": 112862,
+        "likes": 6
+        },
+        {
+        "likes": 8,
+        "item_id": 113436
+        },
+        {
+        "item_id": 8961,
+        "likes": 46
+        },
+        {
+        "likes": 14,
+        "item_id": 66953
+        },
+        {
+        "likes": 1,
+        "item_id": 66949
+        },
+        {
+        "item_id": 49667,
+        "likes": 3
+        },
+        {
+        "likes": 19,
+        "item_id": 9614
+        },
+        {
+        "likes": 11,
+        "item_id": 16571
+        },
+        {
+        "item_id": 151037,
+        "likes": 3
+        },
+        {
+        "likes": 3,
+        "item_id": 18751
+        },
+        {
+        "likes": 1,
+        "item_id": 18754
+        },
+        {
+        "likes": 1,
+        "item_id": 18759
+        },
+        {
+        "item_id": 18747,
+        "likes": 2
+        },
+        {
+        "likes": 1,
+        "item_id": 59817
+        },
+        {
+        "item_id": 202383,
+        "likes": 2
+        },
+        {
+        "likes": 1,
+        "item_id": 217906
+        },
+        {
+        "likes": 1,
+        "item_id": 217907
+        },
+        {
+        "likes": 2,
+        "item_id": 30731
+        },
+        {
+        "item_id": 29669,
+        "likes": 1
+        },
+        {
+        "likes": 2,
+        "item_id": 30661
+        },
+        {
+        "item_id": 16794,
+        "likes": 1
+        },
+        {
+        "likes": 18,
+        "item_id": 16776
+        }
+        ]
+    return likes;
+};
+
+export default likesList;
