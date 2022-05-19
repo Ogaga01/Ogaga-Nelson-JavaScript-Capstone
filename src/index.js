097dca93fd91afe1a713a450aec104b3f066e33a
Ogaga <iyaraogaga69@gmail.com>
2022-05-19 09:37:20 +0100

    Fix linters elements

--- src/index.js
+++ src/index.js
@@ -13,47 +13,44 @@ const likes = [];
 let images = [];
 
 const updateLikes = async () => {
-    const appID = 'NY2YXaf6kAE8tAIQDCPo';
-    await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/likes`)
-      .then((response) => response.json())
-      .then((data) => {
-        data.forEach((article) => {
-          likes.push(article.likes);
-        });
-      });
-  };
-
-  const like = async (id) => {
-    const appID = 'NY2YXaf6kAE8tAIQDCPo';
-    await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/likes`, {
-      method: 'Post',
-      headers: {
-        'content-type': 'application/json; charset=UTF-8',
-      },
-      body: JSON.stringify({
-        item_id: id,
-      }),
-    }).then(() => {
-      updateLikes();
-    });
-  };
-
-  const getLikeElements = () => {
-    const hearts = document.querySelectorAll('.like');
-    const likeCounter = document.querySelectorAll('.like-count');
-    hearts.forEach((heart, index) => {
-      let counter = 0;
-      heart.addEventListener('click', (e) => {
-        e.preventDefault();
-        like(images[index].id);
-        counter += 1;
-        likeCounter[index].innerHTML = `${likes[index] + counter} Likes`;
+  const appID = 'NY2YXaf6kAE8tAIQDCPo';
+  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/likes`)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((article) => {
+        likes.push(article.likes);
       });
     });
-  };
+};
 
-  
- 
+const like = async (id) => {
+  const appID = 'NY2YXaf6kAE8tAIQDCPo';
+  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/likes/`, {
+    method: 'Post',
+    headers: {
+      'content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+      item_id: id,
+    }),
+  }).then(() => {
+    updateLikes();
+  });
+};
+
+const getLikeElements = () => {
+  const hearts = document.querySelectorAll('.like');
+  const likeCounter = document.querySelectorAll('.like-count');
+  hearts.forEach((heart, index) => {
+    let counter = 0;
+    heart.addEventListener('click', (e) => {
+      e.preventDefault();
+      like(images[index].id);
+      counter += 1;
+      likeCounter[index].innerHTML = `${likes[index] + counter} Likes`;
+    });
+  });
+};
 
 const baseUrl = 'https://api.artic.edu/api/v1/artworks?limit=15';
 const getImages = async () => {
@@ -67,8 +64,8 @@ const getImages = async () => {
     date: image.date_start,
     artist: image.artist_title,
   })).filter((image) => image.image_id !== null && image.artist !== null);
-  
-  const imageString = images.map((img, index) =>  `
+
+  const imageString = images.map((img, index) => `
   <article class="article-style">
    <img class="image-style" src="https://www.artic.edu/iiif/2/${img.image_id}/full/843,/0/default.jpg"
        alt="image of artwork">
@@ -92,9 +89,7 @@ const getImages = async () => {
 };
 
 window.onload = () => {
-    updateLikes();
-  getLikeElements()
-  
+//   getLikeElements();
+  updateLikes();
   setTimeout(() => getImages(), 2000);
-  ;
 };
