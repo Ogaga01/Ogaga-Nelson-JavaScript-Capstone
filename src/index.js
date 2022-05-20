766f36e7dc97326fe261278cf955b4d33ef614fc
mandelatuks <manderat206@gmail.com>
2022-05-20 10:48:20 +0200

    Add comment feature,counter feature,popUp for comment

--- src/index.js
+++ src/index.js
@@ -8,8 +8,123 @@ logo.src = img;
 
 const artContainer = document.getElementById('art-container');
 const artContent = document.createElement('div');
+const popupContainer = document.querySelector('.bg-popup');
+const commentForm = document.querySelector('form');
+const messageContainer = document.querySelector('.message');
+const commentsCounterParagraph = document.querySelector('.comments-counter');
+const grab = (e) => document.getElementById(e);
 
 let images = [];
+
+const message = (text) => {
+  messageContainer.innerHTML = text;
+  setTimeout(() => {
+    messageContainer.innerHTML = '';
+  }, 2000);
+};
+
+const renderComment = (arr) => {
+  const commentContainer = document.querySelector('.comments-container');
+  commentContainer.innerHTML = '';
+  if (arr) {
+    arr.forEach((item) => {
+      commentContainer.innerHTML += `
+      <span class="bg-blue-400 p-2 mt-3 text-lg text-white mr-0">${item.creation_date}</span>
+      <span class="bg-blue-500 p-2 mt-3 text-lg text-white">${item.username}:</span>
+      <span class="p-2 mt-3 text-lg">${item.comment}</span>
+      <br>
+      <br>`;
+    });
+  }
+};
+
+const fetchComments = async (id) => {
+  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Km2IRmmIjbL7vNKDRpvP/comments?item_id=${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // if(data.status)
+      if (data.length > 0) {
+        commentsCounterParagraph.innerHTML = `Comment (${data.length})`;
+      } else if (data.error.status === 400) {
+        commentsCounterParagraph.innerHTML = 'Comment (0)';
+      } else {
+        commentsCounterParagraph.innerHTML = data.error;
+      }
+
+      renderComment(data);
+    });
+};
+
+const createNewComment = async (newObject, itemId = null) => {
+  const appID = 'Km2IRmmIjbL7vNKDRpvP';
+  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/comments/`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; Charset=UTF-8',
+    },
+    body: JSON.stringify(newObject),
+  })
+    .then((response) => {
+      if (response.status === 201) {
+        message('Comment successfully added.');
+        fetchComments(itemId);
+      }
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+};
+const handleCommentForm = (itemId) => {
+  commentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = grab('name');
+    const comment = grab('comment');
+
+    if (name.value && comment.value !== '') {
+      const newComment = {
+        item_id: itemId,
+        username: name.value,
+        comment: comment.value,
+      };
+      createNewComment(newComment, itemId);
+      name.value = '';
+      comment.value = '';
+    } else {
+      message('please fill all fields');
+    }
+  });
+};
+const openPopup = (images) => {
+  const trigger = document.querySelectorAll('.comment');
+  trigger.forEach((element, index) => {
+    element.addEventListener('click', () => {
+      popupContainer.classList.remove('hidden');
+
+      images.forEach(async (img, imgIndex) => {
+        if (imgIndex === index) {
+          grab('popup-image').src = `https://www.artic.edu/iiif/2/${img.image_id}/full/843,/0/default.jpg`;
+          const cardDetails = document.querySelector('.details-container');
+          const details = ` <h2 class="title">${img.title},
+       ${img.date}</h2>
+     <h3 class="artist">${img.artist}</h3>`;
+          cardDetails.innerHTML = details;
+
+          fetchComments(img.id);
+          handleCommentForm(img.id);
+        }
+      });
+    });
+  });
+};
+
+const closePopup = () => {
+  const closeButton = grab('closeIcon');
+  closeButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    popupContainer.classList.add('hidden');
+  });
+};
 
 const baseUrl = 'https://api.artic.edu/api/v1/artworks?limit=15';
 const getImages = async () => {
@@ -24,6 +139,8 @@ const getImages = async () => {
     artist: image.artist_title,
   })).filter((image) => image.image_id !== null && image.artist !== null);
 
+  const paintCounter = document.querySelector('.paint-counter');
+  paintCounter.innerHTML = images.length;
   const imageString = images.map((img) => `
   <article class="article-style">
    <img class="image-style" src="https://www.artic.edu/iiif/2/${img.image_id}/full/843,/0/default.jpg"
@@ -44,6 +161,9 @@ const getImages = async () => {
        </article>`).join('');
   artContainer.innerHTML = imageString;
   artContainer.appendChild(artContent);
+
+  openPopup(images);
+  closePopup();
 };
 
 window.onload = () => {
